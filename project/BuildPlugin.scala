7b9f56d7992945c5d04637ad0e664a4e414257fb
jvican <jorgevc@fastmail.es>
2017-11-15 17:24:10 +0100

    Sync up with improvements in sbt-pom-reader

--- project/BuildPlugin.scala
+++ project/BuildPlugin.scala
@@ -1,3 +1,8 @@
 package nailgun.build
+import com.typesafe.sbt.pom.SbtPomKeys
+import sbt.Def
 
-object NailgunBuild extends com.typesafe.sbt.pom.PomBuild
+object NailgunBuild extends com.typesafe.sbt.pom.PomBuild {
+  override def projectDefinitions(baseDirectory: sbt.File) =
+    super.projectDefinitions(baseDirectory).map(p => p.settings(SbtPomKeys.isJavaOnly := true))
+}
