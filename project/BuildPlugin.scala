f024510631d5191dba301f9b09b17889f8479db7
jvican <jorgevc@fastmail.es>
2017-12-19 13:42:11 +0100

    Update sourcedep

--- project/BuildPlugin.scala
+++ project/BuildPlugin.scala
@@ -1,8 +1,8 @@
 package nailgun.build
-import com.typesafe.sbt.pom.SbtPomKeys
+import ch.epfl.scala.sbt.pom.{PomBuild, SbtPomKeys}
 import sbt.Def
 
-object NailgunBuild extends com.typesafe.sbt.pom.PomBuild {
+object NailgunBuild extends PomBuild {
   override def projectDefinitions(baseDirectory: sbt.File) =
     super.projectDefinitions(baseDirectory).map(p => p.settings(SbtPomKeys.isJavaOnly := true))
 }
