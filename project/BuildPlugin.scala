2a5b04abc8a70949a2c493dca030d9a38d9359af
jvican <jorgevc@fastmail.es>
2018-01-26 15:46:56 +0100

    Adding sbt-dynver to nailgun

--- project/BuildPlugin.scala
+++ project/BuildPlugin.scala
@@ -1,8 +1,22 @@
 package nailgun.build
 import ch.epfl.scala.sbt.pom.{PomBuild, SbtPomKeys}
-import sbt.Def
+import sbt.{Def, Keys}
+import sbtdynver.DynVer
+import sbtdynver.DynVerPlugin.autoImport.{dynver, dynverCurrentDate, dynverGitDescribeOutput}
 
 object NailgunBuild extends PomBuild {
+  val ourDynVerInstance = sbt.settingKey[sbtdynver.DynVer]("dynver2")
+  val dynverSettings: Seq[Def.Setting[_]] = List(
+    ourDynVerInstance := DynVer(Some(Keys.baseDirectory.in(sbt.ThisBuild).value)),
+    dynver := ourDynVerInstance.value.version(new java.util.Date),
+    dynverGitDescribeOutput :=
+      ourDynVerInstance.value.getGitDescribeOutput(dynverCurrentDate.value),
+  )
+
+  override def settings: Seq[Def.Setting[_]] = super.settings ++ dynverSettings
+
   override def projectDefinitions(baseDirectory: sbt.File) =
-    super.projectDefinitions(baseDirectory).map(p => p.settings(SbtPomKeys.isJavaOnly := true))
+    super.projectDefinitions(baseDirectory)
+      .map(p => p.settings(SbtPomKeys.isJavaOnly := true))
+      .map(p => p.settings(Keys.version := ourDynVerInstance.value.version(new java.util.Date)))
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,4 @@
 val SbtPomReader = RootProject(uri("git://github.com/scalacenter/sbt-pom-reader#2a6ebd37af79b6d9c295876aa611b722ea7dcfd4"))
 dependsOn(ProjectRef(SbtPomReader.build, "sbt-pom-reader"))
 addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0")
+addSbtPlugin("com.dwijnand" % "sbt-dynver" % "2.0.0")
