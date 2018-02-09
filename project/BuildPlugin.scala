e9bbd776de1c329a705b85eec1fd252c660c49a0
jvican <jorgevc@fastmail.es>
2018-02-09 02:04:14 +0100

    Update release-related build stuff

--- project/BuildPlugin.scala
+++ project/BuildPlugin.scala
@@ -1,9 +1,30 @@
 package nailgun.build
 import ch.epfl.scala.sbt.pom.{PomBuild, SbtPomKeys}
-import sbt.Def
+import sbt.{Def, Keys, ThisBuild}
+import sbtdynver.DynVer
+import sbtdynver.DynVerPlugin.{autoImport => DynVerKeys}
 
 object NailgunBuild extends PomBuild {
-  override def projectDefinitions(baseDirectory: sbt.File) =
-    super.projectDefinitions(baseDirectory).map(p => p.settings(projectSettings))
-  val projectSettings: Seq[Def.Setting[_]] = List(SbtPomKeys.isJavaOnly := true)
+  override def projectDefinitions(baseDirectory: sbt.File) = {
+    super.projectDefinitions(baseDirectory).map { p =>
+      p.settings(projectSettings)
+    }
+  }
+
+  val newVersion: Def.Initialize[String] = Def.settingDyn {
+    val ref = Keys.thisProjectRef.value
+    Def.setting(Keys.version.in(ThisBuild).in(ref).value)
+  }
+
+  val dynverInstance: Def.Initialize[DynVer] = Def.settingDyn {
+    val ref = Keys.thisProjectRef.value
+    Def.setting(DynVer(Some((Keys.baseDirectory in ThisBuild in ref).value)))
+  }
+
+  val projectSettings: Seq[Def.Setting[_]] = List(
+    SbtPomKeys.isJavaOnly := true,
+    // Necessary because of weird settings init of sbt-pom-reader
+    Keys.version := newVersion.value,
+    DynVerKeys.dynverInstance := dynverInstance.value
+  )
 }
