af4d2e7f45cc35b57f5f6dec9255fa0a83bd573b
Jorge Vicente Cantero <jorgevc@fastmail.es>
2019-01-08 23:52:21 +0100

    Follow symlinks when detecting installation directory
    This is required in macOS to have `bloop server` detect correctly
    `.jvmopts` files. This patch is required because `brew` installs
    symlinks in /usr/local/bin to the cellar directories.

--- pynailgun/ng.py
+++ pynailgun/ng.py
@@ -1025,7 +1025,7 @@ def main():
 
     if cmd == "server":
         print("Starting the bloop server... this may take a few seconds")
-        basedir = os.path.dirname(os.path.abspath(sys.argv[0]))
+        basedir = os.path.dirname(os.path.realpath(os.path.abspath(sys.argv[0])))
         server_location = os.path.join(basedir, "blp-server")
         if not os.path.isfile(server_location):
             if options.server_location:
